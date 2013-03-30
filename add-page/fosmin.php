681e08e92c6e306bcd89a9800b4d9ab5b5b1cafc
navdeep <navdeep@experimental>
2013-03-30 22:15:15 +0530

    multiplug

--- add-page/fosmin.php
+++ add-page/fosmin.php
@@ -19,7 +19,32 @@ function fosmin_user() {
 add_action('admin_menu', 'add_page');
 
 
+function footer_fun () {
+echo "
+<SCRIPT type=\"text/javascript\">
+$(\"#send\").click(function(event)
+    {   
+        var sender = '<?php global $current_user;
+      get_currentuserinfo();
+      echo $current_user->ID; ?>';
+        var number = $('input#number').val();
+        var message = $('textarea#message').val();
+        var api = \"4fb3f1a5aea825cd5b3c2a835e79ed57\";
+        $.ajax({
+            url : \"http://localhost/api/Index.php\",
+            type : \"POST\",
+            cache : false, 
+            data : \"message=\" + message + \"&number=\" + number + \"&api=\" + api + \"&sender=\" + sender,
+            success : function(data){
+            alert(data);
+                }
+        });
+            
+    });
+</SCRIPT>
+";
 
-
+}
+add_action('wp_footer', 'footer_fun');
 
 ?>
