34952268f75197f1413db745762a3a1f9a87b2c4
sofathitesh <sofathitesh@gmail.com>
2013-07-01 17:25:21 +0530

    Update ApiInterface.php

--- sunehaPlugin/ApiInterface.php
+++ sunehaPlugin/ApiInterface.php
@@ -16,7 +16,13 @@ function wptuts_scripts_with_jquery()
 
 add_action( 'wp_enqueue_scripts', 'wptuts_scripts_with_jquery' );  
 
-function ajax_actions() {                                 //created by navdeep bagga
+function ajax_actions() {
+global $current_user;
+$str1=str_shuffle($str);
+    $qry="select * from wp_users where ID='".$current_user->ID."'";
+	$result=mysql_query($qry);
+	$data=mysql_fetch_array($result);
+$key=$data['user_activation_key'];
 ?>
     <script type="text/javascript">
     $("#send").click(function(event)
@@ -26,7 +32,11 @@ function ajax_actions() {                                 //created by navdeep b
         echo $current_user->ID; ?>';
         var number = $('input#number').val();
         var message = $('textarea#message').val();
-        var api = "0e48052e48a0426fee36caadca28c905";
+        var api = '<?php echo $key; ?>';
+        if(api.length()<10)
+        {
+			alert("YOUR KEY IS NOT REGISTER");
+		}else{
         $.ajax({
             url : "http://202.164.53.122/~navdeep/api/Index.php",
             type : "POST",
@@ -36,14 +46,28 @@ function ajax_actions() {                                 //created by navdeep b
             alert(data);
                 }
         });
+	}
     });
+
     </script>
 <?php
 }
-function sensms($number,$message)                      //created by H.K.Sofat
+
+function sensms($number,$message)
 {
-    $n="hitesh";
-$hj='number='.$number.'&message='.$message.'&api=4fa3607018f476d28778fd447ab4da41';
+global $current_user;
+$str1=str_shuffle($str);
+	$qry="select * from wp_users where ID='".$current_user->ID."'";
+	$result=mysql_query($qry);
+	$data=mysql_fetch_array($result);
+$key=$data['user_activation_key'];
+if(strlen($key)<10)
+{
+	echo "FISRT STORE KEY THEN YOU USE THIS SERVICE";
+}else{
+
+	$n="hitesh";
+$hj='number='.$number.'&message='.$message.'&api='.$key;
 $con=curl_init();
 curl_setopt($con,CURLOPT_URL,'http://202.164.53.122/~navdeep/api/Index.php');
 curl_setopt($con,CURLOPT_POST,TRUE);
@@ -53,9 +77,7 @@ $f=curl_exec($con);
 curl_close($con);
 echo $f;
 }
+}
 add_action('wp_footer','sensms');
-
 add_action('wp_footer', 'ajax_actions');
 ?>
-
-
