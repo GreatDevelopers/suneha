65ced0d512552b6834d0e2e7c69dbc860ff59e8c
sofathitesh <sofathitesh@gmail.com>
2013-07-01 17:22:22 +0530

    Update account_information.php

--- add-page/account_information.php
+++ add-page/account_information.php
@@ -1,22 +1,41 @@
-<?php 
+<?php
+if(isset($_POST['keyvalue']))
+{
+      stroekey($_POST['keyvalue']);
+}
 
 global $current_user;
       get_currentuserinfo();
-
-      //echo "Hey ". $current_user->user_login . "\n";echo "<br>";
       $result = mysql_query("SELECT user_activation_key,user_status  FROM wp_users
 WHERE ID='$current_user->ID'");
 
 while($row = mysql_fetch_array($result))
   {
-  _e("<font color=\"#3D3D3D\"><b><p style=\"font-family:Helvetica; font-size:14px;\">Your Account Information</p></b></font>"); 
+  _e("<font color=\"#3D3D3D\"><b><p style=\"font-family:Helvetica; font-size:14px;\">Your Account Information</p></b></font>");
   echo "<li>";
-  //_e('<p style="font-family:Helvetica; font-size:12px;">You left with '.$row['user_status']." SMS.</p>");echo "</li><li>";
-  _e('<p style="font-family:Helvetica; font-size:12px;">Your API key is '.$row['user_activation_key']."</p>" );
-  echo "</li>";
+//  _e('<p style="font-family:Helvetica; font-size:12px;">You left with '.$row['user_status']." SMS.</p>");echo "</li><li>";
+if(strlen($row['user_activation_key'])<10)
+{
+	$str="4fa3607018f476d28778fd447ab4da41";
+	$str1=str_shuffle($str);
+	if(strlen($row['user_activation_key'])<10)
+	{
+		echo "PLEASE ENTER YOUR KEY AND SUBMIT. THEN YOU USE THIS PLUGIN<br>";
+		echo "<h3>YOUR PLUGIN KEY IS</h3> ".$str1;
+echo "<legend><label><form action='admin.php?page=FOSMIN' method='post'>ENTER PLUGIN KEY</label><input type='text' name='keyvalue' style='width:270px'><input type='submit' name='keystore' value='SAVE'></form></legend>";
 
+	}else{
+		echo "<h3>YOUR PLUGIN KEY IS SAVE</h3> " .$row['user_activation_key'];
+	}
+	
+}else{
+	
+_e('<p style="font-family:Helvetica; font-size:12px;">Your API key is<b> '.$row['user_activation_key']."</b></p>" );
+  echo "</li>";
 }
-?>	
+}
+?>
+
 
   
 
